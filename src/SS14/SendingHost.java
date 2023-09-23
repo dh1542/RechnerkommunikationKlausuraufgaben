285e97750a0c40882865a9a4406fc1052bcb8b6f
Dominik Fedora Desktop <dominik151099@outlook.de>
2023-09-23 13:01:46 +0200

    First solution for SS14

--- src/SS14/SendingHost.java
+++ src/SS14/SendingHost.java
@@ -1,23 +1,64 @@
 package SS14;
 
 public class SendingHost extends CommonHost {
+    int nextSQN = 1;
+    int base = 1;
+
+    Pkt[] packets = new Pkt[1000];
 
 
+    private Pkt genPkt(int nextSQN, Pkt packet){
+        Pkt out = new Pkt();
+        out.seqnum = nextSQN;
+        out.acknum = packet.acknum;
+        out.payload = packet.payload;
+        out.checksum = packet.checksum;
+        return out;
+    }
 
-    int seq = 0;
+
 
     @Override
     boolean fromAbove(Pkt pkt) {
-        return false;
+        if(nextSQN >= base + W){
+            return false;
+        }
+        else{
+            packets[nextSQN] = genPkt(nextSQN, pkt);
+            transmit(packets[nextSQN]);
+            if(base == nextSQN){
+                startTimer();
+            }
+            nextSQN++;
+        }
+        return true;
     }
 
     @Override
     void receive(Pkt pkt) {
-
+        if(!bitError(pkt)){
+            if(pkt.seqnum == base - 1){
+                for(int i = base; i < nextSQN; i++){
+                    transmit(packets[i]);
+                }
+            }
+            else if(base <= pkt.seqnum && pkt.seqnum < nextSQN){
+                base = pkt.acknum + 1;
+                if(nextSQN == base){
+                    stopTimer();
+                }
+                else{
+                    startTimer();
+                }
+            }
+        }
     }
 
     @Override
     void timeout() {
-
+        for(int i = base; i < nextSQN; i++){
+            transmit(packets[i]);
+        }
+        startTimer();
     }
 }
